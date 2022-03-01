1e72119a6bc982e9db9a9c4ddcafd06f54bc1ec5
ElectroChop24 <oscar.dt024@gmail.com>
2022-03-01 14:20:51 -0500

    Se tiene avanzado el menu principal

---
+++ Funciones/funciones_curso.py
@@ -0,0 +1,10 @@
+"""
+En este archivo crearemos funciones que impliquen; las consulta de información de los arhivos.
+-->>Crear nuevo curso;
+
+-->>Mostrar todos los cursos registrados;
+
+-->>Consultar estudiantes matriculados en un curso;
+
+-->>Porcentaje de estudiantes que gano y perdio un curso;
+"""
---
+++ Funciones/funciones_estudiante.py
@@ -0,0 +1,15 @@
+"""
+En este archivo crearemos funciones que impliquen; la modificación de arhivos.
+-->>Agregar estudiante;
+    Se le pediran al usuario dos tipos de datos, numero de identificación (int), y nombre, verificaremos que los datos.
+    
+-->>Matricular estudiante en un curso;
+
+-->>Mostrar todos los estudiantes registrados;
+
+-->>Consultar promedio de estudiante;
+
+-->>Consultar materias matriculadas por un estudiante;
+
+-->> Retroceder;
+"""
